f5d4f19c4eb4b9d81a0380a77e405afdc16fd319
david.castillo <dave.castillo@definitivelogic.com>
2022-08-07 22:14:51 -0400

    front-end -test pull real data, import bootstrap

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,72 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import {
+  Card, CardSubtitle, CardBody,
+  CardTitle, CardText, Button
+} from "reactstrap"
+
+const App = () => {
+
+  const [contacts, setContacts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch('api/contacts')
+      .then(response => response.json())
+      .then(data => {
+        setContacts(data);
+        setLoading(false);
+      })
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-intro">
+          <h2>Contacts Demo</h2>
+        </div>
       </header>
+      <div>
+        {contacts.map(contact =>
+          <div key={contact.id}>
+            <Card
+              style={{
+                width: '18rem'
+              }}
+            >
+              <img
+                alt="Sample"
+                src="https://picsum.photos/300/200"
+              />
+              <CardBody>
+                <CardTitle tag="h5">
+                  {contact.firstName}_{contact.lastName}
+                </CardTitle>
+                <CardSubtitle
+                  className="mb-2 text-muted"
+                  tag="h6"
+                >
+                  {contact.email}
+                </CardSubtitle>
+                <CardText>
+                  {contact.phone}
+                </CardText>
+                <Button>
+                  Button
+                </Button>
+              </CardBody>
+            </Card>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
